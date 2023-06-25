3e2660be96ac852381868653b8c80c3a5b21aff9
Arturo Guerra <arturoalejandroguerram@gmail.com>
2023-06-24 19:10:47 -0500

    Primer commit

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const data = [
+    {
+		id: 1,
+		img: 'https://jumbocolombiaio.vtexassets.com/arquivos/ids/400435-800-800?v=637962799114230000&width=800&height=800&aspect=true',
+		nameProduct: 'Arroz Diana',
+		price: 3000,
+		quantity: 1,
+	},
+	{
+		id: 2,
+		img: 'https://jumbocolombiaio.vtexassets.com/arquivos/ids/480163-300-300?v=1774047811&width=300&height=300&aspect=true',
+		nameProduct: 'Papel higiénico Familia',
+		price: 18700,
+		quantity: 1,
+	},
+	{
+		id: 3,
+		img: 'https://jumbocolombiaio.vtexassets.com/arquivos/ids/192800-300-300?v=1774026993&width=300&height=300&aspect=true',
+		nameProduct: 'Huevos Gallina Feliz',
+		price: 7180,
+		quantity: 1,
+	},
+	{
+		id: 4,
+		img: 'https://jumbocolombiaio.vtexassets.com/arquivos/ids/418390-300-300?v=1774006475&width=300&height=300&aspect=true',
+		nameProduct: 'Aceite Gourmet Canola',
+		price: 17500,
+		quantity: 1,
+	},
+	{
+		id: 5,
+		img: 'https://jumbocolombiaio.vtexassets.com/arquivos/ids/268441-800-800?v=637830749949730000&width=800&height=800&aspect=true',
+		nameProduct: 'Pan Bimbo',
+		price: 6200,
+		quantity: 1,
+	},
+];
