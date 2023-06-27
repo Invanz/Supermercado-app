367858bfbde56c819e6815b6caeca5554d14d6e9
Arturo Guerra <arturoalejandroguerram@gmail.com>
2023-06-26 20:38:36 -0500

    Falta lógica dentro de App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,26 +10,9 @@ function App() {
 	const [total, setTotal] = useState(0);
 	const [countProducts, setCountProducts] = useState(0);
 
-
 	return (
 		<>
 		<Login />
-			{/* <Header
-				allProducts={allProducts}
-				setAllProducts={setAllProducts}
-				total={total}
-				setTotal={setTotal}
-				countProducts={countProducts}
-				setCountProducts={setCountProducts}
-			/>
-			<ProductList
-				allProducts={allProducts}
-				setAllProducts={setAllProducts}
-				total={total}
-				setTotal={setTotal}
-				countProducts={countProducts}
-				setCountProducts={setCountProducts}
-			/> */}
 		</>
 	);
 }
